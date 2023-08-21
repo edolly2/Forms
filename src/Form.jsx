f95b9230fa9be143ae9eb9016e60e2a0a0057316
dollyllama <edolly2@protonmail.com>
2023-08-21 17:03:49 -0500

    ""

---
+++ src/Form.jsx
@@ -0,0 +1,44 @@
+const Form = () => {
+  return (
+    <form>
+      <div className='form-control'>
+        <label htmlFor='' className='form-label'></label>
+        <input type='text' className='form-input' />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='' className='form-label'></label>
+        <input type='text' className='form-input' />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='' className='form-label'></label>
+        <input type='text' className='form-input' />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='' className='form-label'></label>
+        <input type='text' className='form-input' />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='' className='form-label'></label>
+        <input type='text' className='form-input' />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='' className='form-label'></label>
+        <input type='text' className='form-input' />
+      </div>
+      <div className='form-control'>
+        <label htmlFor='' className='form-label'></label>
+        <input type='text' className='form-input' />
+      </div>
+      <div className='form-control'>
+        <textarea></textarea>
+      </div>
+      <div className='form-control'>
+        <button type='submit' className='submit-btn'>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
